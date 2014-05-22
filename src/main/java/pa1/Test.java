70b4615206ef32bf1d76541feaaff63a7e14a255
alexliangshi <alex.liangshi@gmail.com>
2014-05-22 17:48:46 +0800

    update20140522

--- src/main/java/pa1/Test.java
+++ src/main/java/pa1/Test.java
@@ -1,12 +1,39 @@
 package pa1;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import com.meterware.httpunit.HTMLPage;
+import com.meterware.httpunit.GetMethodWebRequest;
+//import com.meterware.httpunit.PostMethodWebRequest;   
+//import com.meterware.httpunit.TableCell;   
+import com.meterware.httpunit.WebConversation;
+import com.meterware.httpunit.WebLink;
+//import com.meterware.httpunit.WebForm;   
+//import com.meterware.httpunit.WebLink;   
+import com.meterware.httpunit.WebRequest;
+import com.meterware.httpunit.WebClient;
+import com.meterware.httpunit.WebResponse;
+//import com.meterware.httpunit.WebTable;   
+import java.lang.String;
+//import java.net.MalformedURLException;
+//import com.meterware.httpunit.*;
+import org.xml.sax.SAXException;
+
 public class Test {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		try {
+			WebConversation wc = new WebConversation();
+			WebRequest req = new GetMethodWebRequest("http://httpunit.sourceforge.net/doc/cookbook.html");
+			WebResponse resp= wc.getResponse(req);
+			resp.getCharacterSet();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+
+			e.printStackTrace();
+		}  
 
 	}
 
